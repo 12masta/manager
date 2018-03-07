02659475451fd4dcf68210e9dde8ab585d279b8b
12masta <stanek.marcinp@gmail.com>
2018-03-07 01:33:29 +0100

    making login user more robust

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  componentWillmount() {
-  const config = {
-    apiKey: 'AIzaSyCMi5-8QcweCuP91S2IkgZYNfHZUWqtU0g',
-    authDomain: 'manager-32667.firebaseapp.com',
-    databaseURL: 'https://manager-32667.firebaseio.com',
-    projectId: 'manager-32667',
-    storageBucket: 'manager-32667.appspot.com',
-    messagingSenderId: '345721350213'
-  };
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCMi5-8QcweCuP91S2IkgZYNfHZUWqtU0g',
+      authDomain: 'manager-32667.firebaseapp.com',
+      databaseURL: 'https://manager-32667.firebaseio.com',
+      projectId: 'manager-32667',
+      storageBucket: 'manager-32667.appspot.com',
+      messagingSenderId: '345721350213'
+    };
 
-  firebase.initializeApp(config);
+    firebase.initializeApp(config);
   }
 
   render() {
