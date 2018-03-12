03af99ddfc929c293ab9874e186679395029bdf1
Marcin <marcin@MacBook-Pro-Aleksandra.local>
2018-03-12 22:03:31 +0100

    building the employee creation form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text> Employee form </Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="555-555-555"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="555-555-555"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="555-555-555"
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }
