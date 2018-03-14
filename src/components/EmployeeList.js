07478cb61a41cc8191e4af983a2fd74cbeeb98e9
Marcin <marcin@MacBook-Pro-Aleksandra.local>
2018-03-14 23:52:09 +0100

    list building in employee list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,43 +1,30 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { ListView, View, Text } from 'react-native';
 import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
   componentWillMount() {
+    this.createDataSource();
+  }
+
+  createDataSource() {
     this.props.employeesFetch();
-    this.createDataSource(this.props);
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.createDataSource(nextProps);
-  }
-
-  createDataSource({ employees }) {
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    });
-
-    this.dataSource = ds.cloneWithRows(employees);
+  renderRow(employee) {
+    return <ListItem employee={employee} />;
   }
 
   render() {
-    console.log(this.props);
     return (
-      <View>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-        <Text>EmployeeList</Text>
-      </View>
+      <FlatList
+        data={this.props.employees}
+        renderItem={this.renderRow}
+        keyExtractor={employee => employee.uid}
+      />
     );
   }
 }
@@ -45,9 +32,9 @@ class EmployeeList extends Component {
 const mapStateToProps = state => {
   const employees = _.map(state.employees, (val, uid) => {
     return { ...val, uid };
-  });
+    });
 
-  return { employees };
+    return { employees };
 };
 
 export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
